170a1e76ffd49ecc5967c8c2c355c085654424f5
Rithesh <ritheshasowda@gmail.com>
2026-04-22 14:27:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("outputs/model.pkl")
+
+st.title("Employee Performance Predictor")
+
+age = st.slider("Age", 20, 60)
+env = st.slider("Environment Satisfaction", 1, 4)
+work = st.slider("Work-Life Balance", 1, 4)
+
+if st.button("Predict"):
+    data = np.array([[age, env, work]])
+    result = model.predict(data)
+    st.success(f"Performance Rating: {result[0]}")
